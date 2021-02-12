7249a654d8b59b4be51e6827a067325eca88016a
Tarudahat <taruzahat@gmail.com>
2021-02-12 18:21:19 +0100

    add score board PC, fix misc. bugs

---
+++ Consoles/NDS/source/structs.c
@@ -0,0 +1,44 @@
+#include <nds.h>
+#include <stdio.h>
+
+struct SaveStruct{
+    u64 total_score;
+    u64 hi_scores[4][5];
+};
+
+struct Player
+{
+    u64 score;
+    char *score_str;
+    u32 anim_delay;
+    u8 current_frame;
+    s16 player_x;
+    s16 player_y;
+    u8 player_state;
+    u8 bridges;
+    s8 speed;
+    u8 collision_x; //only needed for systems with 8x8 tiles
+    u8 collision_y; //only needed for systems with 8x8 tiles
+};
+
+struct Timer
+{
+    u32 delay;
+};
+
+struct enemy_group
+{
+    u8 group_members;
+    u32 anim_delay;
+    u8 current_frame;
+    u16 rotation[20];
+    u8 enemy_id[20];
+    bool collided_with_player[20];
+    s8 enemy_direction[20];
+    s16 enemy_x[20];
+    s16 enemy_y[20];
+    s16 target_x[20];
+    s16 target_y[20];
+    bool can_spawn[20];
+    u8 moves[20];
+};
---
+++ Godot/menus/scores.gd
@@ -0,0 +1,12 @@
+extends TextureRect
+
+func _ready():
+	$ScrollContainer/front_panel/score_board_text.bbcode_text=$ScrollContainer/front_panel/score_board_text.bbcode_text.replace("_total_score_",var2str(SceneGlobals.total_score))
+	for i in range(5):
+		for i2 in range(4):
+			$ScrollContainer/front_panel/score_board_text.bbcode_text=$ScrollContainer/front_panel/score_board_text.bbcode_text.replace("_"+var2str(i)+"_"+var2str(i2)+"_" ,var2str(SceneGlobals.hi_scores[i][i2]))
+
+func _input(_event):
+	if $ScrollContainer/front_panel/button_cancel.pressed==true or Input.is_action_just_pressed("ui_cancel"):
+		# warning-ignore:return_value_discarded
+		get_tree().change_scene("res://menus/title_menu.tscn")
